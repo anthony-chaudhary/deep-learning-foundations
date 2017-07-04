14f4c08d0ea825036a1d1b206add6713875267af
swirlingsand <anthonysarkis@gmail.com>
2017-07-04 12:35:24 -0600

    chatbot processed

--- chatbot/config.py
+++ chatbot/config.py
@@ -16,7 +16,7 @@ See readme.md for instruction on how to run the starter code.
 """
 
 # parameters for processing the dataset
-DATA_PATH = '/Users/Chip/data/cornell movie-dialogs corpus'
+DATA_PATH = 'cornell movie-dialogs corpus'
 CONVO_FILE = 'movie_conversations.txt'
 LINE_FILE = 'movie_lines.txt'
 OUTPUT_FILE = 'output_convo.txt'
@@ -57,3 +57,5 @@ LR = 0.5
 MAX_GRAD_NORM = 5.0
 
 NUM_SAMPLES = 512
+ENC_VOCAB = 24422
+DEC_VOCAB = 24724
